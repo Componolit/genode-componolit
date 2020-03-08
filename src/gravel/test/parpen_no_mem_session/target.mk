5b8533486f07e89e775d1d1b303940d3b6d0d8a8
Alexander Senier <senier@componolit.com>
2020-03-08 21:22:09 +0100

    Add invalid initialization test

---
+++ src/gravel/test/parpen_no_mem_session/target.mk
@@ -0,0 +1,32 @@
+TARGET := parpen_test_no_mem_session
+
+SRC_ADB := \
+	component.adb \
+	parpen-generic_types.adb \
+	parpen-protocol-generic_reply.adb \
+	parpen-protocol-generic_label.adb \
+	rflx_container.adb
+
+SRC_ADS := \
+	parpen.ads \
+	parpen-protocol.ads
+
+ADA_COMPONENTS_DIR = $(REP_DIR)/modules/gravel/components
+
+INC_DIR := \
+	$(ADA_COMPONENTS_DIR)/common/parpen \
+	$(ADA_COMPONENTS_DIR)/common/parpen/generated \
+	$(ADA_COMPONENTS_DIR)/common/parpen/test \
+	$(ADA_COMPONENTS_DIR)/common/parpen/test/no_mem_session \
+
+LIBS := base spark gneiss basalt
+CC_ADA_WARN_STRICT ?=
+
+vpath %.ads \
+	$(ADA_COMPONENTS_DIR)/common/parpen \
+	$(ADA_COMPONENTS_DIR)/common/parpen/generated
+
+vpath %.adb \
+	$(ADA_COMPONENTS_DIR)/common/parpen/test/no_mem_session \
+	$(ADA_COMPONENTS_DIR)/common/parpen/generated \
+	$(ADA_COMPONENTS_DIR)/common/parpen/ \
