281804bb62aba466e531d7ceb22f97df7d820d8c
Alexander Senier <senier@componolit.com>
2020-03-08 21:22:09 +0100

    Add invalid initialization test

--- src/gravel/server/parpen/target.mk
+++ src/gravel/server/parpen/target.mk
@@ -1,7 +1,18 @@
 TARGET := parpen
-SRC_ADB := component.adb
+
+SRC_ADB += \
+	component.adb \
+	rflx_container.adb \
+	parpen-generic_types.adb \
+	parpen-protocol.adb \
+	parpen-protocol-generic_label.adb \
+	parpen-protocol-generic_reply.adb \
+
+SRC_ADS += \
+	parpen.ads \
+
 LIBS := base spark gneiss basalt
 MODULE_DIR = $(REP_DIR)/modules/gravel/components/common/parpen
-INC_DIR += $(MODULE_DIR)
+INC_DIR += $(MODULE_DIR) $(MODULE_DIR)/generated
 CC_ADA_WARN_STRICT ?=
-vpath component.adb $(MODULE_DIR)
+VPATH += $(MODULE_DIR) $(MODULE_DIR)/generated
